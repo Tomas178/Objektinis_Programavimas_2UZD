c01231831933d5153bc0fd02c5f8fbf58e1e00ca
Tomas Petronis <tomaspetronis77gmail.com>
2024-02-26 11:58:11 +0200

    Sutvarkytas funckijos.h failas

--- funkcijos.h
+++ funkcijos.h
@@ -1,6 +1,23 @@
 #ifndef FUNKCIJOS_H
 #define FUNKCIJOS_H
 
+#include <vector>
+#include <string>
 
+struct Studentokai {
+    std::string vardas;
+    std::string pavarde;
+    std::vector<int> namu_darbai;
+    int egzaminas;
+    double mediana;
+    double vidurkis;
+};
+
+double Vidurkis(int nd_kiekis, int nd_suma, int egzaminas);
+double medianosSkaiciavimas(const std::vector<int>& namu_darbai, int nd_kiekis, int egzaminas);
+bool palygintiPagalVarda(const Studentokai &a, const Studentokai &b);
+bool palygintiPagalPavarde(const Studentokai &a, const Studentokai &b);
+bool palygintiPagalVidurki(const Studentokai &a, const Studentokai &b);
+bool palygintiPagalMediana(const Studentokai &a, const Studentokai &b);
 
 #endif
