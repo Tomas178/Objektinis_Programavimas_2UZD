93ce30e3b75d9eddee1005d92848e79720b87ee4
Tomas Petronis <tomaspetronis77gmail.com>
2024-04-21 22:46:47 +0300

    Funkcijos.cpp failui irgi padaryti doxygen komentarai

--- Vector/Sources/funkcijos.cpp
+++ Vector/Sources/funkcijos.cpp
@@ -1,6 +1,12 @@
 #include "../Headers/Zmogus.h"
 #include "../Headers/funkcijos.h"
 
+/**
+ * @brief Studento klasės default'inis konstruktorius.
+ * Vardo ir pavardės nustatymas į tuščią string'ą.
+ * Namų darbų vektorius išvalomas.
+ * Egzamino, medianos ir vidurkio balas nustatomas į 0.
+*/
 Studentas::Studentas() : Zmogus() {
     namu_darbai.clear();
     egzaminas = 0;
@@ -8,6 +14,14 @@ Studentas::Studentas() : Zmogus() {
     vidurkis = 0.0;
 }
 
+/**
+ * @brief Studento klasės konstruktorius.
+ * @param vard Studento vardas.
+ * @param pavard Studento pavardė.
+ * Vardo ir pavardės nustatymas į perduotus parametrus.
+ * Namų darbų vektorius išvalomas.
+ * Egzamino, medianos ir vidurkio balas nustatomas į 0.
+*/
 Studentas::Studentas(const string &vard, const string &pavard) : Zmogus(vard, pavard) {
     namu_darbai.clear();
     egzaminas = 0;
@@ -15,7 +29,13 @@ Studentas::Studentas(const string &vard, const string &pavard) : Zmogus(vard, pa
     vidurkis = 0.0;
 }
 
-//Kopijavimo konstruktorius
+/**
+ * @brief Studento klasės kopijavimo konstruktorius.
+ * @param LaikinasStudentas Studentas, kurio duomenys kopijuojami.
+ * Vardas ir pavardė nukopijuojami.
+ * Namų darbų vektorius nukopijuojamas.
+ * Egzamino, medianos ir vidurkio balas nukopijuojamas.
+*/
 Studentas::Studentas(const Studentas &LaikinasStudentas) : Zmogus(LaikinasStudentas.vardas, LaikinasStudentas.pavarde) {
     namu_darbai = LaikinasStudentas.namu_darbai;
     egzaminas = LaikinasStudentas.egzaminas;
@@ -24,6 +44,13 @@ Studentas::Studentas(const Studentas &LaikinasStudentas) : Zmogus(LaikinasStuden
     //cout << "Kopijavimo konstruktorius suveike" << endl;
 }
 
+/**
+ * @brief Studento klasės perkelimo konstruktorius.
+ * @param LaikinasStudentas Studentas, kurio duomenys perkeliami.
+ * Vardas ir pavardė perkeliami.
+ * Namų darbų vektorius perkeliamas.
+ * Egzamino, medianos ir vidurkio balas perkeliamas.
+*/
 Studentas::Studentas(Studentas &&LaikinasStudentas) noexcept : Zmogus(move(LaikinasStudentas.vardas), move(LaikinasStudentas.pavarde)){
     namu_darbai = move(LaikinasStudentas.namu_darbai);
     egzaminas = move(LaikinasStudentas.egzaminas);
@@ -34,6 +61,11 @@ Studentas::Studentas(Studentas &&LaikinasStudentas) noexcept : Zmogus(move(Laiki
     //cout << "Perkelimo konstruktorius suveike" << endl;
 }
 
+/**
+ * @brief Studento klasės kopijavimo operatorius.
+ * @param LaikinasStudentas Studentas, kurio duomenys kopijuojami.
+ * @return Grąžinamas Studentas su nukopijuotais duomenimis.
+*/
 Studentas& Studentas::operator=(const Studentas &LaikinasStudentas){
     if(this != &LaikinasStudentas){
         vardas = LaikinasStudentas.vardas;
@@ -47,6 +79,11 @@ Studentas& Studentas::operator=(const Studentas &LaikinasStudentas){
     return *this;
 }
 
+/**
+ * @brief Studento klasės perkelimo operatorius.
+ * @param LaikinasStudentas Studentas, kurio duomenys perkeliami.
+ * @return Grąžinamas Studentas su perkeltais duomenimis.
+*/
 Studentas& Studentas::operator=(Studentas &&LaikinasStudentas){
     if(this != &LaikinasStudentas){
         vardas = move(LaikinasStudentas.vardas);
@@ -62,6 +99,12 @@ Studentas& Studentas::operator=(Studentas &&LaikinasStudentas){
     return *this;
 }
 
+/**
+ * @brief Įvesties iš failo operatorius.
+ * @param filename Įvesties failas.
+ * @param LaikinasStudentas Studentas, kuriam bus priskiriami duomenys.
+ * @return Grąžinamas Studentas su nuskaitytais duomenimis.
+*/
 istringstream& operator>>(istringstream& filename, Studentas &LaikinasStudentas){
     string vardas, pavarde;
     if (!(filename >> vardas >> pavarde)) {
@@ -88,6 +131,12 @@ istringstream& operator>>(istringstream& filename, Studentas &LaikinasStudentas)
     return filename;
 }
 
+/**
+ * @brief Įvesties per konsolę operatorius.
+ * @param manual Įvesties srautas.
+ * @param LaikinasStudentas Studentas, kuriam bus priskiriami duomenys.
+ * @return Grąžinamas Studentas su nuskaitytais duomenimis.
+*/
 istream& operator>>(istream& manual, Studentas &LaikinasStudentas){
     if(programos_veikimas == 1){
     string vardas, pavarde;
@@ -162,14 +211,25 @@ istream& operator>>(istream& manual, Studentas &LaikinasStudentas){
     return manual;
 }
 
+/**
+ * @brief Išvesties į ekraną operatorius.
+ * @param console Išvesties srautas.
+ * @param LaikinasStudentas Studentas, kurio duomenys išvedami.
+ * @return Grąžinamas Studentas su išvestais duomenimis.
+*/
 ostream& operator<<(ostream& console, const Studentas &LaikinasStudentas){
     console << left << setw(20) << LaikinasStudentas.Get_Vardas() << setw(20) << LaikinasStudentas.Get_Pavarde() <<
     setw(20) << fixed << setprecision(2) << LaikinasStudentas.Get_Vidurkis() << fixed << setw(20) << LaikinasStudentas.Get_Mediana();
     //cout << "As esu isvedimo i konsole operatoriuje <<" << endl;
     return console;
-
 }
 
+/**
+ * @brief Išvesties į failą operatorius.
+ * @param filename Išvesties failas.
+ * @param LaikinasStudentas Studentas, kurio duomenys išvedami.
+ * @return Grąžinamas Studentas su išvestais duomenimis.
+*/
 ofstream& operator<<(ofstream& filename, const Studentas &LaikinasStudentas){
     stringstream RF;
     RF << left << setw(20) << LaikinasStudentas.Get_Vardas() << setw(20) << LaikinasStudentas.Get_Pavarde() <<
@@ -180,10 +240,21 @@ ofstream& operator<<(ofstream& filename, const Studentas &LaikinasStudentas){
     return filename;
 }
 
+/**
+ * @brief Studento klasės destruktorius.
+ * Namų darbų vektorius išvalomas.
+*/
 Studentas::~Studentas(){
     namu_darbai.clear();
 }
 
+/**
+ * @brief Studento klasės galutinio balo skaičiavimas pagal vidurkį.
+ * @param nd_kiekis Namų darbų kiekis.
+ * @param nd_suma Namų darbų suma.
+ * @param egzaminas Egzamino balas.
+ * @return Grąžinamas galutinis balas pagal vidurkį.
+*/
 double Studentas::Vidurkis(int nd_kiekis, int nd_suma, int egzaminas){
     if(nd_kiekis > 0)
         return 0.4*nd_suma/nd_kiekis + 0.6*egzaminas;
@@ -191,6 +262,13 @@ double Studentas::Vidurkis(int nd_kiekis, int nd_suma, int egzaminas){
         return 0.6*egzaminas;
 }
 
+/**
+ * @brief Studento klasės galutinio balo skaičiavimas pagal medianą.
+ * @param namu_darbai Namų darbų vektorius.
+ * @param nd_kiekis Namų darbų kiekis.
+ * @param egzaminas Egzamino balas.
+ * @return Grąžinamas galutinis balas pagal medianą.
+*/
 double Studentas::medianosSkaiciavimas(const vector<int> &namu_darbai, int nd_kiekis, int egzaminas){
     if(nd_kiekis % 2 == 0 && nd_kiekis > 0)
         return (namu_darbai[nd_kiekis/2-1] + namu_darbai[nd_kiekis/2])/2.0*0.4 + 0.6*egzaminas;
@@ -200,22 +278,41 @@ double Studentas::medianosSkaiciavimas(const vector<int> &namu_darbai, int nd_ki
         return 0.6*egzaminas;
 }
 
+/**
+ * @brief Dviejų studentų palyginimas pagal vardą.
+ * @return Grąžinama true, jei pirmo studento vardas yra žemiau abecėlėje už antro studento vardą. False atvirkščiai.
+*/
 bool palygintiPagalVarda(const Studentas &a, const Studentas &b) {
     return a.Get_Vardas() < b.Get_Vardas();
 }
 
+/**
+ * @brief Dviejų studentų palyginimas pagal pavardę.
+ * @return Grąžinama true, jei pirmo studento pavardė yra žemiau abecėlėje už antro studento pavardę. False atvirkščiai.
+*/
 bool palygintiPagalPavarde(const Studentas &a, const Studentas &b) {
     return a.Get_Pavarde() < b.Get_Pavarde();
 }
 
+/**
+ * @brief Dviejų studentų palyginimas pagal vidurkį.
+ * @return Grąžinama true, jei pirmo studento vidurkis yra mažesnis už antro studento vidurkį. False atvirkščiai.
+*/
 bool palygintiPagalVidurki(const Studentas &a, const Studentas &b) {
     return a.Get_Vidurkis() < b.Get_Vidurkis();
 }
 
+/**
+ * @brief Dviejų studentų palyginimas pagal vidurkį.
+ * @return Grąžinama true, jei pirmo studento mediana yra mažesnė už antro studento medianą. False atvirkščiai.
+*/
 bool palygintiPagalMediana(const Studentas &a, const Studentas &b) {
     return a.Get_Mediana() < b.Get_Mediana();
 }
 
+/**
+ * @brief Pašalinti studentus, kurių galutinis balas >= 5.0.
+*/
 void PasalintiKietusStudentus(vector<Studentas> &Studentai, int norimas_rikiavimas){
     try{
     if (norimas_rikiavimas == 3) {
@@ -248,6 +345,13 @@ void PasalintiKietusStudentus(vector<Studentas> &Studentai, int norimas_rikiavim
     }
 }
 
+/**
+ * @brief Generuoti studentų failą.
+ * @param kiekis Studentų kiekis.
+ * @param nd_kiekis Namų darbų kiekis.
+ * Sugeneruojamas failas su studentų vardais, pavardėmis, namų darbų ir egzamino balais.
+ * Failo pavadinimas: Studentai[kiekis].txt
+*/
 void GeneruotiFaila(int kiekis, int nd_kiekis){
     string FailoPavadinimas = "Studentai" + to_string(kiekis) + ".txt";
     ofstream FF(FailoPavadinimas);
@@ -284,6 +388,14 @@ void GeneruotiFaila(int kiekis, int nd_kiekis){
     }  
 }
 
+/**
+ * @brief Išvesti rezultatus į ekraną arba į failą.
+ * @param pavadinimas Failo pavadinimas.
+ * @param Studentai Studentų vektorius.
+ * @param norima_isvedimo_vieta Norima išvesti rezultatus: 1 - į ekraną, 2 - į failą.
+ * Išvedami studentų vardai, pavardės, galutinis balas pagal vidurkį ir galutinis balas pagal medianą.
+ * Failo pavadinimas: [pavadinimas].txt
+*/
 void IsvestiRezultatus(string pavadinimas, const vector<Studentas> &Studentai, int norima_isvedimo_vieta) {
     if (Studentai.size() > 0) {
         if (norima_isvedimo_vieta == 1) {
@@ -309,6 +421,10 @@ void IsvestiRezultatus(string pavadinimas, const vector<Studentas> &Studentai, i
     }
 }
 
+/**
+ * @brief Generuoti atsitiktinius pažymius studentui intervale [1;10].
+ * @param LaikinasStudentas Studentas, kuriam bus sugeneruojami pažymiai.
+*/
 void GeneruotiPazymius(Studentas &LaikinasStudentas){
     int nd_kiekis = rand() % 10 + 1;
     LaikinasStudentas.ND_clear();
@@ -318,6 +434,11 @@ void GeneruotiPazymius(Studentas &LaikinasStudentas){
     LaikinasStudentas.setEgzaminas(rand() % 10 + 1);
 }
 
+/**
+ * @brief Generuoti atsitiktinį studentą.
+ * @param LaikinasStudentas Studentas, kuriam bus sugeneruojami duomenys.
+ * Sugeneruojamas studento vardas, pavardė, namų darbų ir egzamino balai.
+*/
 void GeneruotiStudenta(Studentas &LaikinasStudentas){
     vector<string> Vardai = {"Tomas", "Matas", "Kasparas", "Algirdas", "Mantas", "Adomas", "Simona", "Gerda", "Jurgita", "Ruta", "Lukas", "Edvardas", "Ernestas", "Rimas"};
     vector<string> Pavardes = {"Petronis", "Semenas", "Ceseviciute", "Poskus", "Sumskis", "Leonardas", "Petronyte", "Serelis", "Kubilius", "Katleris", "Stonkus", "Sabonis"};
